9363d536066075965bd00959e643f98bd9cf90c9
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 19:03:39 +0000

    adding tests for xTotal and yTotal starting at 0

---
+++ puzzle_1/specs/solution_spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe( "Solution", function() {
+
+  var solution = null;
+
+  beforeEach( function() {
+    solution = new Solution();
+  });
+
+  it( "Should have xTotal starting at 0", function() {
+    assert.equal( 0, solution.xTotal );
+  })
+
+  it( "Should have yTotal starting at 0", function() {
+    assert.equal( 0, solution.yTotal );
+  })
+});
