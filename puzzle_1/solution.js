67fa9636a94380555c474a6a37a9ad4992efa5e6
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 22:35:54 +0000

    adding tests and code to do meat fo problem

--- puzzle_1/solution.js
+++ puzzle_1/solution.js
@@ -1,40 +1,57 @@
-var dataParser = require('./data_parser');
+var DataParser = require('./data_parser');
 
 var Solution = function() {
 
   this.xTotal = 0;
   this.yTotal = 0;
+
+  var self = this;
+
+  this.moveX = function( count ) { this.xTotal += count },
+  this.moveY = function( count ) { this.yTotal += count },
+
+  this.right = {
+    turnRight: 'down',
+    turnLeft: 'up',
+    move: function( count ) { self.moveX( count ) }
+  };
+
+  this.left = {
+    turnRight: 'up',
+    turnLeft: 'down',
+    move: function( count ) { self.moveX( -count ) }
+  };
+
+  this.up = {
+    turnRight: 'right',
+    turnLeft: 'left',
+    move: function( count ) { self.moveY( count ) }
+  };
+
+  this.down = {
+    turnRight: 'left',
+    turnLeft: 'right',
+    move: function( count ) { self.moveY( -count ) }
+  };
 }
 
 Solution.prototype = {
 
-  model: {
-    right: {
-      turnRight: 'down',
-      turnLeft: 'up',
-      move: function( count ) { this.x += count }
-    },
-    left: {
-      turnRight: 'up',
-      turnLeft: 'down',
-      move: function( count ) { this.x -= count }
-    },
-    up: {
-      turnRight: 'right',
-      turnLeft: 'left',
-      move: function( count ) { this.y += count }
-    },
-    down: {
-      turnRight: 'left',
-      turnLeft: 'right',
-      move: function( count ) { this.y -= count }
-    }
-  },
-
   solve: function( rawData ) {
 
+    var dataParser = new DataParser();
     var data = dataParser.parseData( rawData );
-    var current_direction
+    var currentDirection = 'up';
+    var nextMovement = dataParser.next();
+
+    while ( nextMovement != undefined ) {
+
+      var nextDirection = nextMovement[0];
+      var nextCount = nextMovement[1];
+
+      currentDirection = this[currentDirection][nextDirection];      this[currentDirection].move( nextCount );
+      nextMovement = dataParser.next();
+    }
   }
 }
 
