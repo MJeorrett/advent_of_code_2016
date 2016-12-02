1b8fe831b94b484e582e85b0c0e710171cee8c81
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 19:14:23 +0000

    added model

--- puzzle_1/solution.js
+++ puzzle_1/solution.js
@@ -2,6 +2,29 @@ var Solution = function() {
 
   this.xTotal = 0;
   this.yTotal = 0;
+
+  this.model = {
+    right: {
+      turnRight: 'down',
+      turnLeft: 'up'
+      move: function( count ) { this.x += count }
+    },
+    left: {
+      turnRight: 'up',
+      turnLeft: 'down',
+      move: function( count ) { this.x -= count }
+    },
+    up: {
+      turnRight: 'right',
+      turnLeft: 'left',
+      move: function( count ) { this.y += count }
+    },
+    down: {
+      turnRight: 'left',
+      turnLeft: 'right',
+      move: function( count ) { this.y -= count }
+    }
+  }
 }
 
 module.exports = Solution;
