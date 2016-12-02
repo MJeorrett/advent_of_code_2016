590e82fe2bffb8f4b10b390a7aac467594b38ac6
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 18:42:39 +0000

    adding getValues function

--- puzzle_1/data_parser.js
+++ puzzle_1/data_parser.js
@@ -3,6 +3,13 @@ var dataParser = {
 
     var items = data.split( ", " );
     return items;
+  },
+  getValues: function( entry ) {
+
+    var direction = entry[0];
+    var countString = entry.substring( 1 );
+    var count = parseInt( countString );
+    return [ direction, count ];
   }
 };
 
