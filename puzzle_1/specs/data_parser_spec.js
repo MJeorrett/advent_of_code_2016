2ab9dc647f700e516e7e3d31209cae802dab8b8f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-02 18:47:02 +0000

    adding test for parseValues()

--- puzzle_1/specs/data_parser_spec.js
+++ puzzle_1/specs/data_parser_spec.js
@@ -11,4 +11,10 @@ describe( "Data Parser", function() {
   it( "should split value into direction and count", function() {
     assert.deepEqual( ["R", 14], dataParser.getValues( "R14" ) );
   })
+
+  it( "should return array of directions and counts from parseValues -- test 1", function() {
+    var input = "R14, L3";
+    var expected = [["R", 14], ["L", 3]];
+    assert.deepEqual( expected, dataParser.parseValues( input ) );
+  });
 });
