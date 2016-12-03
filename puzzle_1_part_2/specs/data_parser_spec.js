57d5308afbab041c432fa24c36040857a1849995
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 17:07:37 +0000

    adding test for parse()

--- puzzle_1_part_2/specs/data_parser_spec.js
+++ puzzle_1_part_2/specs/data_parser_spec.js
@@ -8,4 +8,17 @@ describe( "Data Parser", function() {
     expected = [ "R", 45 ];
     assert.deepEqual( expected, dataParser.parseItem( input ) );
   });
+
+  it( "should contain array of parsed items from parse()", function() {
+    intput = "R4, R2, L192, R5, L2, R53";
+    expected = [
+      ['R', 4],
+      ['R', 2],
+      ['L', 192],
+      ['R', 5],
+      ['L', 2],
+      ['R', 53],
+    ];
+    assert.deepEqual( expected, dataParser.parse( input ) );
+  });
 });
