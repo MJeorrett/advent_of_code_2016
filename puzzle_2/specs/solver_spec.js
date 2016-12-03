bc98b06f52214c0ff415ab36de3f9f215eeb3a20
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 13:41:35 +0000

    adding comprehensive tests for moveDirectionFrom() function

--- puzzle_2/specs/solver_spec.js
+++ puzzle_2/specs/solver_spec.js
@@ -5,41 +5,77 @@ describe( "Solver", function() {
 
   var solver = null;
 
+  var row1 = [1, 2, 3];
+  var row2 = [4, 5, 6];
+  var row3 = [7, 8, 9];
+
+  var column1 = [1, 4, 7];
+  var column2 = [2, 5, 8];
+  var column3 = [3, 6, 9];
+
   beforeEach( function() {
 
     solver = new Solver();
   });
 
-  it ( "should return [0, 0] from rowAndColumnOfNumber( 1 )", function() {
-    assert.deepEqual( [0, 0], solver.rowAndColumnOfNumber( 1 ) );
+  it( "should return same number when moving up from row 1", function() {
+
+    for ( var num of row1 ) {
+      assert.equal( num, solver.moveDirectionFrom( "U", num ) );
+    }
   });
 
-  it ( "should return [0, 2] from rowAndColumnOfNumber( 3 )", function() {
-    assert.deepEqual( [0, 2], solver.rowAndColumnOfNumber( 3 ) );
+  it( "should return same number when moving down from row 3", function() {
+
+    for ( var num of row3 ) {
+      assert.equal( num, solver.moveDirectionFrom( "D", num ) );
+    }
   });
 
-  it ( "should return [1, 1] from rowAndColumnOfNumber( 5 )", function() {
-    assert.deepEqual( [1, 1], solver.rowAndColumnOfNumber( 5 ) );
+  it( "should return same number when moving left from column 1", function() {
+
+    for ( var num of column1 ) {
+      assert.equal( num, solver.moveDirectionFrom( "L", num ) );
+    }
   });
 
-  it ( "should return [2, 1] from rowAndColumnOfNumber( 8 )", function() {
-    assert.deepEqual( [2, 1], solver.rowAndColumnOfNumber( 8 ) );
+  it( "should return same number when moving right from column 3", function() {
+
+    for ( var num of column3 ) {
+      assert.equal( num, solver.moveDirectionFrom( "R", num ) );
+    }
   });
 
-  it( "should return same coordinates when moving up from row 0", function() {
-    assert.deepEqual( [0, 1], solver.moveDirectionFrom( "U", [0, 1] ) );
+  it( "should return number + 1 when moving right from columns 1 and 2", function() {
+    var input = column1.concat( column2 );
+
+    for ( var num of input ) {
+      assert.equal( num + 1, solver.moveDirectionFrom( "R", num ) );
+    }
   });
 
-  it( "should return same coordinates when moving down from row 2", function() {
-    assert.deepEqual( [2, 1], solver.moveDirectionFrom( "D", [2, 1] ) );
+  it( "should return number - 1 when moving left from columns 2 and 3", function() {
+    var input = column2.concat( column3 );
+
+    for ( var num of input ) {
+      assert.equal( num + 1, solver.moveDirectionFrom( "R", num ) );
+    }
   });
 
-  it( "should return same coordinates when moving right from column 2", function() {
-    assert.deepEqual( [2, 2], solver.moveDirectionFrom( "R", [2, 2] ) );
+  it( "should return number + 3 when moving down from rows 1 and 2", function() {
+    var input = row1.concat( row2 );
+
+    for ( var num of input ) {
+      assert.equal( num + 3, solver.moveDirectionFrom( "R", num ) );
+    }
   });
 
-  it( "should return same coordinates when moving left from column 0", function() {
-    assert.deepEqual( [2, 0], solver.moveDirectionFrom( "L", [2, 0] ) );
+  it( "should return number - 3 when moving down from rows 2 and 3", function() {
+    var input = row2.concat( row3 );
+
+    for ( var num of input ) {
+      assert.equal( num - 3, solver.moveDirectionFrom( "R", num ) );
+    }
   });
 
   // it( "should return answer '1985' to example problem 1", function() {
