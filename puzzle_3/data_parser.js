7c160bfaaf88395aa689cf2d87dade43122ca07d
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 14:43:46 +0000

    adding initial implementation

---
+++ puzzle_3/data_parser.js
@@ -0,0 +1,18 @@
+var dataParser = function( data ) {
+
+  var lines = data.split( "\n" );
+
+  var result = lines.map( function( line ) {
+
+    var numberStrings = line.split( " " );
+    
+    var numbers = numberStrings.map( function( numberString ){
+      return parseInt( numberString );
+    });
+    return numbers;
+  });
+
+  return result;
+};
+
+module.exports = dataParser;
