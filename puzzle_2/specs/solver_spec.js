018151050e33967f7561f67d56fa59c8ddec3362
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 13:18:52 +0000

    adding further tests for rowAndColumnOFNumber

--- puzzle_2/specs/solver_spec.js
+++ puzzle_2/specs/solver_spec.js
@@ -14,6 +14,18 @@ describe( "Solver", function() {
     assert.deepEqual( [0, 0], solver.rowAndColumnOfNumber( 1 ) );
   });
 
+  it ( "should return [0, 2] from rowAndColumnOfNumber( 3 )", function() {
+    assert.deepEqual( [0, 2], solver.rowAndColumnOfNumber( 3 ) );
+  });
+
+  it ( "should return [1, 1] from rowAndColumnOfNumber( 5 )", function() {
+    assert.deepEqual( [1, 1], solver.rowAndColumnOfNumber( 5 ) );
+  });
+
+  it ( "should return [2, 1] from rowAndColumnOfNumber( 8 )", function() {
+    assert.deepEqual( [2, 1], solver.rowAndColumnOfNumber( 8 ) );
+  });
+
   // it( "should return answer '1985' to example problem 1", function() {
   //
   //   var input = "ULL\nRRDDD\nLURDL\nUUUUD";
