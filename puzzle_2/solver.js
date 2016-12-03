1db51f695c156d8a55b88d7ce25cbe8869ee4faf
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 14:06:23 +0000

    adding moveDirectionFrom()

--- puzzle_2/solver.js
+++ puzzle_2/solver.js
@@ -5,24 +5,87 @@ var Solver = function() {};
 Solver.prototype = {
 
   keypad: [
-    [1, 2 ,3],
+    [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
   ],
 
-  rowAndColumnOfNumber: function( number ) {
+  moveDirectionFrom: function( direction, startNumber ) {
 
-    var row = -1;
-    var column = -1;
+    var result = startNumber;
+    var topRow = 0;
+    var bottomRow = 2;
+    var rightColumn = 2;
+    var leftColumn = 0;
 
-    while ( column === -1 ) {
+    switch ( direction ) {
+      case 'U':
+        if ( this.rowOfNumber( startNumber ) > topRow ) { result -= 3 }
+        break;
 
-      row++;
-      var aRow = this.keypad[row];
-      var column = aRow.indexOf( number );
+      case 'D':
+        if ( this.rowOfNumber( startNumber ) < bottomRow ) { result += 3 }
+        break;
+
+      case 'L':
+        if ( this.columnOfNumber( startNumber ) > leftColumn ) { result -= 1 }
+        break;
+
+      case 'R':
+        if ( this.columnOfNumber( startNumber ) < rightColumn ) { result += 1 }
+        break;
     }
 
-    return [ row , column ];
+    return result;
+  },
+
+  numberRowsAndColumns: {
+    '1': {
+      row: 0,
+      column: 0
+    },
+    '2': {
+      row: 0,
+      column: 1
+    },
+    '3': {
+      row: 0,
+      column: 2
+    },
+    '4': {
+      row: 1,
+      column: 0
+    },
+    '5': {
+      row: 1,
+      column: 1
+    },
+    '6': {
+      row: 1,
+      column: 2
+    },
+    '7': {
+      row: 2,
+      column: 0
+    },
+    '8': {
+      row: 2,
+      column: 1
+    },
+    '9': {
+      row: 2,
+      column: 2
+    },
+  },
+
+  rowOfNumber: function( number ) {
+
+    return this.numberRowsAndColumns[ number.toString() ].row;
+  },
+
+  columnOfNumber: function( number ) {
+
+    return this.numberRowsAndColumns[ number.toString() ].column;
   }
 };
 
