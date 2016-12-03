a2003f63eed2e0db19d1940c54436455c72b3e63
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 13:16:58 +0000

    adding first test for rowAndColumnOfNumber

--- puzzle_2/specs/solver_spec.js
+++ puzzle_2/specs/solver_spec.js
@@ -3,12 +3,23 @@ var Solver = require('../solver');
 
 describe( "Solver", function() {
 
-  it( "should return answer '1985' to example problem 1", function() {
+  var solver = null;
 
-    var input = "ULL\nRRDDD\nLURDL\nUUUUD";
-    var solver = new Solver();
-    var actual = solver.solve( input );
-    assert.equal( 1985, actual );
+  beforeEach( function() {
+
+    solver = new Solver();
   });
+
+  it ( "should return [0, 0] from rowAndColumnOfNumber( 1 )", function() {
+    assert.deepEqual( [0, 0], solver.rowAndColumnOfNumber( 1 ) );
+  });
+
+  // it( "should return answer '1985' to example problem 1", function() {
+  //
+  //   var input = "ULL\nRRDDD\nLURDL\nUUUUD";
+  //   var solver = new Solver();
+  //   var actual = solver.solve( input );
+  //   assert.equal( 1985, actual );
+  // });
 
 });
