01def4d36ef8d5fe4c351c52d76de42faf3b027a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 17:04:27 +0000

    amending test

--- puzzle_1_part_2/specs/data_parser_spec.js
+++ puzzle_1_part_2/specs/data_parser_spec.js
@@ -1,10 +1,11 @@
 var assert = require('assert');
+var dataParser = require('../data_parser');
 
 describe( "Data Parser", function() {
 
   it( "should return array containing turn and distance from parseItem()", function() {
     input = "R45"
     expected = [ "R", 45 ];
-    assert.deepEqual( expected = dataParser.parseItem( input ) );
+    assert.deepEqual( expected, dataParser.parseItem( input ) );
   });
 });
