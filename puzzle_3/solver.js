a997c1d479fd2c5cffae8221075907faabfc56f7
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 16:00:42 +0000

    testing all permutations

--- puzzle_3/solver.js
+++ puzzle_3/solver.js
@@ -15,7 +15,17 @@ var solver = {
 
     var data = dataParser( dataString );
     var answer = data.reduce( function( count, dataRow ) {
-      if ( this.solveRow( dataRow ) ) {
+
+      var permutation1 = dataRow;
+      var permutation2 = [ dataRow[1], dataRow[2], dataRow[0] ];
+      var permutation3 = [ dataRow[2], dataRow[0], dataRow[1] ];
+
+      var perm1True = this.solveRow( permutation1 );
+      var perm2True = this.solveRow( permutation2 );
+      var perm3True = this.solveRow( permutation3 );
+
+
+      if ( perm1True && perm2True && perm3True ) {
         count++;
       }
 
