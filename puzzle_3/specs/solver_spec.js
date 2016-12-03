9e3adbb84d6b77e7b7b918efa64ccadf9c8265b3
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 16:02:09 +0000

    updating test names to reflect tests

--- puzzle_3/specs/solver_spec.js
+++ puzzle_3/specs/solver_spec.js
@@ -3,17 +3,17 @@ var solver = require('../solver');
 
 describe( "Solver", function() {
 
-  it( "should return false from solveRow([5, 10, 25])", function() {
+  it( "should return false from solveRow([5, 20, 25])", function() {
 
     assert.equal( false, solver.solveRow( [5, 20, 25] ) );
   });
 
-  it( "should return false from solveRow([4, 10, 25])", function() {
+  it( "should return false from solveRow([4, 20, 25])", function() {
 
     assert.equal( false, solver.solveRow( [4, 20, 25] ) );
   });
 
-  it( "should return true from solveRow([6, 10, 25])", function() {
+  it( "should return true from solveRow([6, 20, 25])", function() {
 
     assert.equal( true, solver.solveRow( [6, 20, 25] ) );
   });
