9a9fa070ceb0c7f0c54d3c7cbc34f952667009f2
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-03 09:16:54 +0000

    adding tests for point

---
+++ puzzle_1/specs/point_spec.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+
+describe( "Point", function() {
+
+  var myPoint = null;
+
+  beforeEach( function() {
+
+    myPoint = new Point();
+  });
+
+  it( "should start with x = 0", function() {
+
+    assert.equal( 0, point.x );
+  });
+
+  it( "should start with x = 0", function() {
+
+    assert.equal( 0, point.x );
+  });
+
+  it( "should increase x by positive value passed to moveX()", function() {
+    myPoint.moveX( 4 );
+    assert( 4, myPoint.x );
+  });
+
+  it( "should increase y by positive value passed to moveY()", function() {
+    myPoint.moveY( 16 );
+    assert( 16, myPoint.y );
+  });
+
+  it( "should decrease x by negative value passed to moveX()", function() {
+    myPoint.moveX( -5 );
+    assert( -5, myPoint.x );
+  });
+
+  it( "should decrease y by negative value passed to moveY()", function() {
+    myPoint.moveY( -32 );
+    assert( -32, myPoint.y );
+  });
+
+});
