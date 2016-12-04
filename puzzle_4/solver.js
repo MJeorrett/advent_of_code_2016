b7771e221ab4cc6a7aabfa2fff8838d10713356a
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 19:35:46 +0000

    adding getChecksum()

--- puzzle_4/solver.js
+++ puzzle_4/solver.js
@@ -1,4 +1,5 @@
 var solver = {
+
   getLetterCounts: function( letters ) {
     var lettersArr = letters.split( "" );
     var letterCounts = {};
@@ -13,6 +14,42 @@ var solver = {
     }
 
     return letterCounts;
+  },
+
+  getChecksum: function( letterCounts ) {
+
+    var letterCountsArray = [];
+
+    for ( var letter in letterCounts) {
+      var count = letterCounts[ letter ];
+      letterCountsArray.push( [ count, letter ] );
+    }
+
+    letterCounts = letterCountsArray.sort( function( letterCount1, letterCount2 ) {
+      var count1 = letterCount1[0];
+      var count2 = letterCount2[0];
+
+      if ( count1 > count2 ) return -1;
+      if ( count2 > count1 ) return 1;
+
+      var letter1 = letterCount1[1];
+      var letter2 = letterCount2[1];
+
+      if ( letter1 < letter2 ) return -1;
+      if ( letter2 < letter1 ) return 1;
+
+      return 0;
+    });
+
+    var result = "";
+
+    for ( var i = 0; i < 5; i++ ) {
+      var letterCount = letterCountsArray[i];
+      var letter = letterCount[1];
+      result += letter;
+    }
+
+    return result;
   }
 };
 
