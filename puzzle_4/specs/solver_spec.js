7ea07704b9d571e233be87cf5cee3bf8d9995b4b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 19:12:11 +0000

    adding tests for getChecksum()

--- puzzle_4/specs/solver_spec.js
+++ puzzle_4/specs/solver_spec.js
@@ -26,4 +26,28 @@ describe( "Solver", function() {
     expected = { t: 2, o: 3, a: 2, l: 3, y: 1, r: 2, e: 1, m: 1 };
     assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
+
+  it( "should return the correct checksum from letter counts for example 1", function() {
+    var input = { a: 5, b: 3, y: 1, x: 1, z: 1 };
+    expected = "abxyz";
+    assert.equal( expected, solver.getChecksum( input ) );
+  });
+
+  it( "should return the correct checksum from letter counts for example 1", function() {
+    var input = { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1 };
+    expected = "abcde";
+    assert.equal( expected, solver.getChecksum( input ) );
+  });
+
+  it( "should return the correct checksum from letter counts for example 1", function() {
+    var input = { n: 1, o: 3, t: 1, a: 2, r: 2, e: 1, l: 1, m: 1 };
+    expected = "oarel";
+    assert.equal( expected, solver.getChecksum( input ) );
+  });
+
+  it( "should return the correct checksum from letter counts for example 1", function() {
+    var input = { t: 2, o: 3, a: 2, l: 3, y: 1, r: 2, e: 1, m: 1 };
+    expected = "loart";
+    assert.equal( expected, solver.getChecksum( input ) );
+  });
 });
