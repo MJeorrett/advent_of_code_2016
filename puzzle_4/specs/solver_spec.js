c66dcbc6fb6994522cf328540c62211906127657
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 19:06:07 +0000

    fixing to use deepEqual()

--- puzzle_4/specs/solver_spec.js
+++ puzzle_4/specs/solver_spec.js
@@ -6,24 +6,24 @@ describe( "Solver", function() {
   it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
     var input = "aaaaabbbzyx";
     expected = { a: 5, b: 3, y: 1, x: 1, z: 1 };
-    assert.equal( expected, solver.getLetterCounts( input ) );
+    assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
   it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
     var input = "abcdefgh";
     expected = { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1 };
-    assert.equal( expected, solver.getLetterCounts( input ) );
+    assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
   it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
     var input = "notarealroom";
     expected = { n: 1, o: 3, t: 1, a: 2, r: 2, e: 1, l: 1, m: 1 };
-    assert.equal( expected, solver.getLetterCounts( input ) );
+    assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
   it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
     var input = "totallyrealroom";
     expected = { t: 2, o: 3, a: 2, l: 3, y: 1, r: 2, e: 1, m: 1 };
-    assert.equal( expected, solver.getLetterCounts( input ) );
+    assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 });
