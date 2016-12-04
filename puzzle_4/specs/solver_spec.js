4fe87c4d4ff947ce382f25757af8e470d3364702
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 19:09:06 +0000

    correcting names of tests

--- puzzle_4/specs/solver_spec.js
+++ puzzle_4/specs/solver_spec.js
@@ -9,19 +9,19 @@ describe( "Solver", function() {
     assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
-  it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
+  it( "should return object containing letter counts from getLetterCounts( example_2 )", function() {
     var input = "abcdefgh";
     expected = { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1 };
     assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
-  it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
+  it( "should return object containing letter counts from getLetterCounts( example_3 )", function() {
     var input = "notarealroom";
     expected = { n: 1, o: 3, t: 1, a: 2, r: 2, e: 1, l: 1, m: 1 };
     assert.deepEqual( expected, solver.getLetterCounts( input ) );
   });
 
-  it( "should return object containing letter counts from getLetterCounts( example_1 )", function() {
+  it( "should return object containing letter counts from getLetterCounts( example_4 )", function() {
     var input = "totallyrealroom";
     expected = { t: 2, o: 3, a: 2, l: 3, y: 1, r: 2, e: 1, m: 1 };
     assert.deepEqual( expected, solver.getLetterCounts( input ) );
