e6de9cddd7d2ac7322fe1ee98367b98c938b5775
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-06 09:16:04 +0000

    adding tests based on example

---
+++ puzzle_6/specs/solver_spec.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+describe( "Solver", function() {
+
+  it( "should return the correct first letter from example 1", function() {
+
+    var input = "ederatsrnnstvvde";
+    assert.equal( "e", solver.mostCommon( input ) );
+  });
+
+  it( "should return the correct first letter from example 2", function() {
+
+    var input = "eraatsdastvenrvn";
+    assert.equal( "a", solver.mostCommon( input ) );
+  });
+
+  it( "should return the correct first letter from example 3", function() {
+
+    var input = "dvnaertssnestdra";
+    assert.equal( "s", solver.mostCommon( input ) );
+  });
+});
